cdcfbad542a09306ab70955cc8834488f0f1e25c
Kurt_W <email-nummer1@gmx.de>
2019-01-18 00:19:35 +0100

    Added show_Chats

--- Interfaces/show_Chats.php
+++ Interfaces/show_Chats.php
@@ -1,8 +1,21 @@
 <?php
 	include 'connect.php';
 
-	
+	if(isset($_GET['id']))
+	{
+		$query = "SELECT * FROM dpn.chats where `user_ID` = ".$_GET['id'].";";
+
+	}
 
 	
+	$result = mysqli_query($con, $query);
+
+	$id;
+
+	while($row = mysqli_fetch_row($result))
+	{
+		$id = $row[0];
+		echo $row[0]"<br>";
+	}
 
 ?>
