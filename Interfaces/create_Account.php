95a41da7a8b0ec3043776cfaca7ecf5090c3fabf
Kurt_W <email-nummer1@gmx.de>
2019-01-24 08:17:49 +0100

    Update create_Account.php

--- Interfaces/create_Account.php
+++ Interfaces/create_Account.php
@@ -1,25 +1,20 @@
 <?php
 	include 'connect.php';
-
 	if(isset($_GET['username']) && isset($_GET['email']) && isset($_GET['password']))
 	{
 		$query = "SELECT * FROM dpn.account WHERE `EMAIL` = '" .$_GET['email']."';";
 		$result = mysqli_query($con, $query);
-		if($result == null)
+		if($result)
 		{
-
+			echo "Email already exists";	
+		}
+		else 
+		{
 			$query = "INSERT INTO dpn.user (`NAME`) VALUES ('".$_GET['username']."');";
 			$result = mysqli_query($con, $query);	
 
 			$query = "INSERT INTO dpn.account (`user_ID`,`USERNAME`,`PASSWORD`,`EMAIL`) VALUES ('".mysqli_insert_id($con)."','".$_GET['username']."','".$_GET['password']."','".$_GET['email']."');";
 			$result = mysqli_query($con, $query);
 		}
-		else 
-		{
-			echo "Email already exists";
-		}
-
 	}
-	
-
 ?>
