d4e01e79c323580dd2801fa379fe817b9a214e47
Kurt_W <email-nummer1@gmx.de>
2019-01-24 03:19:31 +0100

    Email doubleing protection

--- Interfaces/create_Account.php
+++ Interfaces/create_Account.php
@@ -3,11 +3,21 @@
 
 	if(isset($_GET['username']) && isset($_GET['email']) && isset($_GET['password']))
 	{
-		$query = "INSERT INTO dpn.user (`NAME`) VALUES ('".$_GET['username']."');";
-		$result = mysqli_query($con, $query);	
-
-		$query = "INSERT INTO dpn.account (`user_ID`,`USERNAME`,`PASSWORD`,`EMAIL`) VALUES ('".mysqli_insert_id($con)."','".$_GET['username']."','".$_GET['password']."','".$_GET['email']."');";
+		$query = "SELECT * FROM dpn.account WHERE `EMAIL` = '" .$_GET['email']."';";
 		$result = mysqli_query($con, $query);
+		if($result == null)
+		{
+
+			$query = "INSERT INTO dpn.user (`NAME`) VALUES ('".$_GET['username']."');";
+			$result = mysqli_query($con, $query);	
+
+			$query = "INSERT INTO dpn.account (`user_ID`,`USERNAME`,`PASSWORD`,`EMAIL`) VALUES ('".mysqli_insert_id($con)."','".$_GET['username']."','".$_GET['password']."','".$_GET['email']."');";
+			$result = mysqli_query($con, $query);
+		}
+		else 
+		{
+			echo "Email already exists";
+		}
 
 	}
 	
